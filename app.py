e71dfcdce0effb25752ededb33d2c8fa27c2e9f6
Phillip Knezevich <phknezev@redhat.com>
2025-04-04 18:37:11 +0000

    functional changes added

---
+++ app.py
@@ -0,0 +1,29 @@
+# app.py
+import sys
+import asyncio
+import producer
+import consumer
+from dotenv import load_dotenv
+
+load_dotenv()
+
+async def main():
+    if len(sys.argv) < 2:
+        print("Usage: python app.py <produce|consume>")
+        return
+
+    command = sys.argv[1].lower()
+
+    if command == "produce":
+        # Call produce.py's async produce function
+        await producer.produce()
+
+    elif command == "consume":
+        # Call the async consumer function
+        await consumer.consume()
+
+    else:
+        print("Invalid argument. Use 'produce' or 'consume'.")
+
+if __name__ == "__main__":
+    asyncio.run(main())
